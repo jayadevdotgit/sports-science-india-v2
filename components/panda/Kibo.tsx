59c5717c5b416664903375febcb7d4bf772de9d4
jayadevdotgit <JAYADEVPRADHAN9@GMAIL.COM>
2026-07-30 00:32:01 +0530

    Kibo set to 250px

--- components/panda/Kibo.tsx
+++ components/panda/Kibo.tsx
@@ -9,7 +9,7 @@ import { useBlink } from "./hooks/useBlink";
 import { useIdle } from "./hooks/useIdle";
 import { useWave } from "./hooks/useWave";
 
-const KIBO_SIZE = 150;
+const KIBO_SIZE = 250;
 
 export default function Kibo() {
   const { position, savePosition, ready } = useKibo();
